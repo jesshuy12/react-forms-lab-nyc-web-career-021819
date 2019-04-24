24bd73d5410d937a29d31f036d2fab1af4fcb14d
Jessy Hu <jesshuy12@gmail.com>
2019-04-24 11:39:19 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",  // original username state
+      password: ""  // original password state
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  login = (event) => {
+    event.preventDefault()
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(event)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.login}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,31 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
+
   constructor() {
     super();
+    this.state = {
+      tweet: "" //og state is empty
+    };
+  }
 
-    this.state = {};
+  newTweet = (event) => {
+    this.setState({  //setting new state for tweet on event
+      tweet: event.target.value // new value is the target value of the event
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+          <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={(event) => this.newTweet(event)}
+          value={this.state.tweet}
+          />{this.props.maxChars - this.state.tweet.length}
       </div>
     );
   }
